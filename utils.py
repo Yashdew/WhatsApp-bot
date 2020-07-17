063fac75dfafcb619c6005baa68a1de1b8e82676
Yash Dewangan <47397638+Yashdew@users.noreply.github.com>
2020-07-18 00:07:47 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "small-talk-wgunps-1df2ad308ac2.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = API_KEY
 
 import dialogflow_v2 as dialogflow
 dialogflow_session_client = dialogflow.SessionsClient()
